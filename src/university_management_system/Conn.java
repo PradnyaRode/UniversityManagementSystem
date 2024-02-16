c12ec55460be6c22f005444ef926adc342a8c2b4
unknown <pradnya.mrode@gmail.com>
2024-02-16 11:48:29 +0530

    added

---
+++ src/university_management_system/Conn.java
@@ -0,0 +1,19 @@
+package university_management_system;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///universitymanagementsystem", "root", "root");
+            s = c.createStatement();      
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
